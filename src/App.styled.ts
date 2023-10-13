829aa52c13c4754dd421b5ee114d390cca725be9
Munchicken1 <soojong1996@gmail.com>
2023-10-13 11:47:54 -0500

    Add Another Region Selector Page

--- src/App.styled.ts
+++ src/App.styled.ts
@@ -1,6 +1,11 @@
 import styled from "styled-components";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
-export const AppContainer = styled.div`
+interface IProps {
+  $isHome: boolean;
+}
+
+export const HomeContainer = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -39,36 +44,40 @@ export const ModalLink = styled.a`
   }
 `;
 
-export const ModalPrimaryButton = styled.button`
+export const ModalPrimaryButton = styled.a`
   padding: 20px 80px;
   border: none;
   border-radius: 0.8rem;
   color: black;
   background-color: white;
   font-weight: 600;
-  width: 100%;
+  width: 190px;
   margin-top: 50px;
   font-size: 18px;
   cursor: pointer;
   transition: all 0.5s ease-out;
+  text-align: center;
+  text-decoration: none;
 
   &:hover {
     background-color: black;
     color: white;
   }
 `;
-export const ModalSecondaryButton = styled.button`
+export const ModalSecondaryButton = styled.a`
   padding: 20px 80px;
   border: none;
   border-radius: 0.8rem;
   color: black;
   background-color: white;
   font-weight: 600;
-  width: 100%;
+  width: 190px;
   margin-top: 20px;
   font-size: 18px;
   cursor: pointer;
   transition: all 0.5s ease-out;
+  text-align: center;
+  text-decoration: none;
 
   &:hover {
     background-color: black;
@@ -82,14 +91,91 @@ export const BrandLogo = styled.img`
   max-width: 350px;
   transform: translateX(-50%);
 `;
-export const Footer = styled.div`
+export const Footer = styled.div<IProps>`
   position: absolute;
   left: 50%;
   bottom: 5%;
   transform: translateX(-50%);
-  color: #dddddd;
+  ${(props) => (props.$isHome ? `color: #eeeeee;` : `color: #333333;`)};
+  transition: all 0.5s ease-out;
 `;
 export const FooterText = styled.p`
   font-size: 12px;
   margin: 0;
+`;
+
+// Region Selector Page Styles
+
+export const RegionContainer = styled.div<IProps>`
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  background: #f0f0f3;
+`;
+export const RegionContentContainer = styled.div`
+  min-height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const RegionHeaderText = styled.p`
+  font-size: 24px;
+  font-weight: 600;
+  margin-bottom: 50px;
+`;
+export const RegionSelectorContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 50px;
+  min-width: 300px;
+`;
+export const RegionItem = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  padding: 30px 30px;
+  min-width: 50px;
+  text-align: center;
+  margin: 20px;
+  border-radius: 1.5rem;
+  box-shadow: -4px -4px 6px #ffffff, 4px 4px 6px rgba(174, 174, 192, 0.4),
+    4px 4px 6px #ffffff, -4px -4px 6px rgba(174, 174, 192, 0.4);
+  transition: all 0.5s ease-out;
+
+  &:hover {
+    box-shadow: -4px -4px 6px rgba(174, 174, 192, 0.4), 4px 4px 6px #ffffff,
+      4px 4px 6px rgba(174, 174, 192, 0.4), -4px -4px 6px #ffffff;
+    transition: all 0.5s ease-out;
+  }
+`;
+export const BackButton = styled.span`
+  position: absolute;
+  bottom: 15%;
+  left: 50%;
+  transform: translateX(-50%);
+  padding: 10px;
+  cursor: pointer;
+  border-radius: 2rem;
+
+  box-shadow: -4px -4px 6px #ffffff, 4px 4px 6px rgba(174, 174, 192, 0.4),
+    4px 4px 6px #ffffff, -4px -4px 6px rgba(174, 174, 192, 0.4);
+  transition: all 0.5s ease-out;
+
+  &:hover {
+    box-shadow: -4px -4px 6px rgba(174, 174, 192, 0.4), 4px 4px 6px #ffffff,
+      4px 4px 6px rgba(174, 174, 192, 0.4), -4px -4px 6px #ffffff;
+  }
+`;
+export const BackIcon = styled(ArrowBackIcon)`
+  width: 40px !important;
+  height: 40px !important;
+`;
+export const DescText = styled.p`
+  color: #333333;
+  font-size: 12px;
 `;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import Logo from "./assets/madecalogo.png";
+import { useState } from "react";
+import Logo from "./assets/madecalogo_white.png";
+import ColorLogo from "./assets/madecalogo.png";
 import {
-  AppContainer,
+  HomeContainer,
   ModalContentContainer,
   ModalHeaderText,
   ModalBodyText,
@@ -10,26 +12,72 @@ import {
   BrandLogo,
   Footer,
   FooterText,
+  RegionContainer,
+  RegionContentContainer,
+  RegionHeaderText,
+  RegionSelectorContainer,
+  RegionItem,
+  BackButton,
+  BackIcon,
+  DescText,
 } from "./App.styled";
 
 function App() {
+  const [isHome, setIsHome] = useState(true);
+  const pageToggleHandler = () => {
+    setIsHome(!isHome);
+  };
   return (
-    <AppContainer>
-      <BrandLogo src={Logo} />
-      <ModalContentContainer>
-        <ModalHeaderText>Choose your Country/Region</ModalHeaderText>
-        <ModalBodyText>You are visiting madecaderma.com.</ModalBodyText>
-        <ModalBodyText>
-          Would you like to update your localization?
-        </ModalBodyText>
-        <ModalPrimaryButton>US & Canada</ModalPrimaryButton>
-        <ModalSecondaryButton>International / EN</ModalSecondaryButton>
-        <ModalLink>Choose another country / region</ModalLink>
-      </ModalContentContainer>
-      <Footer>
+    <>
+      {isHome ? (
+        <HomeContainer>
+          <BrandLogo src={Logo} />
+          <ModalContentContainer>
+            <ModalHeaderText>Choose your Country/Region</ModalHeaderText>
+            <ModalBodyText>You are visiting madecaderma.com.</ModalBodyText>
+            <ModalBodyText>
+              Would you like to update your localization?
+            </ModalBodyText>
+            <ModalPrimaryButton href="https://www.madeca.com">
+              US & Canada
+            </ModalPrimaryButton>
+            <ModalSecondaryButton href="http://www.madecaderma.com">
+              International / EN
+            </ModalSecondaryButton>
+            <ModalLink onClick={pageToggleHandler}>
+              Choose another country / region
+            </ModalLink>
+          </ModalContentContainer>
+        </HomeContainer>
+      ) : (
+        <RegionContainer $isHome={isHome}>
+          <BrandLogo src={ColorLogo} />
+          <BackButton onClick={pageToggleHandler}>
+            <BackIcon />
+          </BackButton>
+          <RegionContentContainer>
+            <RegionHeaderText>Choose your Country/Region</RegionHeaderText>
+            <RegionSelectorContainer>
+              <RegionItem>Europe</RegionItem>
+              <RegionItem>Korea</RegionItem>
+              <RegionItem>Japan</RegionItem>
+            </RegionSelectorContainer>
+            <RegionSelectorContainer>
+              <RegionItem>멕시코</RegionItem>
+              <RegionItem>기타</RegionItem>
+              <RegionItem>등등</RegionItem>
+            </RegionSelectorContainer>
+            <DescText>
+              These buttons are intended as placeholders, designed to remain
+              non-functional.
+            </DescText>
+          </RegionContentContainer>
+        </RegionContainer>
+      )}
+      <Footer $isHome={isHome}>
         <FooterText>Madeca Copyright Goes Here</FooterText>
       </Footer>
-    </AppContainer>
+    </>
   );
 }
 
